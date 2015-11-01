b5adeffa7cbbf8af552cbbfa143c803123bb5e68
L00R00 <1305650957@qq.com>
2015-11-01 22:05:54 +0800

    11

---
+++ aa.php
@@ -0,0 +1,13 @@
+<?php
+$s="New  ways made by sublime_text_3!";
+
+echo "hello world";
+echo "<br>";
+echo "This is PHP!";
+echo "<br>";
+echo "New ways!";
+echo "<br>";
+echo "why?";
+echo "<br>";
+echo $s	;
+?>
